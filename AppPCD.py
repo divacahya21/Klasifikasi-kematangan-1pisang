718cac304e18937fa2f82e9ebf9e35043c69521d
divacahya21 <divacahyafatikhah@gmail.com>
2025-06-03 05:42:13 +0700

    Update AppPCD.py

--- AppPCD.py
+++ AppPCD.py
@@ -32,7 +32,7 @@ fitur_order = joblib.load("model/fitur_order.pkl")
 # Sidebar
 # ------------------------------
 with st.sidebar:
-    st.image("images/pisang_sample.jpg", width=200)
+    st.image("images/pisang_sample.jpg", width=500)
     st.markdown("### Pengaturan")
     model_option = st.selectbox("Pilih Model Klasifikasi", ["Random Forest", "SVM"])
     st.markdown("---")
